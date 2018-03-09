53b8c9a48a8097da5f0eee69c618ee338448c73c
MAIN-PC\Administrator <yuansun235@163.com>
2018-03-09 12:08:58 +0800

    First Commit

---
+++ src/give/GiveController.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package give;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * FXML Controller class
+ *
+ * @author noahwebb
+ */
+public class GiveController implements Initializable {
+    @FXML
+    private MediaView mediaView;
+    @FXML
+    private CheckBox muted;
+
+    private MediaPlayer curMediaPlayer;
+    private int curVid;
+    private File[] vidFiles;
+    private boolean isPaused;
+
+    Stage mainStage;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        vidFiles = new File("./src/give/media").listFiles();
+        curVid = 0;
+        isPaused = false;
+        if (vidFiles == null || vidFiles.length == 0) {
+            //handle error
+            System.exit(1);
+        }
+        initMediaPlayer();
+
+        Platform.runLater(() -> mediaView.getScene().getWindow().setOnCloseRequest(event -> {
+            mediaView.getMediaPlayer().stop();
+            mainStage.show();
+            hideTray();
+        }));
+    }
+
+    public void initMediaPlayer() {
+
+        String curVidName = vidFiles[curVid].getName();
+        Media media = new Media(getClass().getResource("media/" + curVidName).toExternalForm());
+        if (curMediaPlayer != null) {
+            curMediaPlayer.stop();
+        }
+        curMediaPlayer = new MediaPlayer(media);
+//        curMediaPlayer.setStartTime(javafx.util.Duration.ZERO);
+        curMediaPlayer.setOnReady(() -> {
+            double len = media.getDuration().toSeconds();
+            System.out.println(len);
+            curMediaPlayer.setStartTime(Duration.seconds(0));
+            curMediaPlayer.setStopTime(Duration.seconds(len - 1));
+        });
+        curMediaPlayer.setOnEndOfMedia(() -> {
+            System.out.println("initializing new media player");
+            initMediaPlayer();
+        });
+        curVid = (curVid + 1) % vidFiles.length;
+        mediaView.setMediaPlayer(curMediaPlayer);
+        curMediaPlayer.play();
+    }
+
+    public void backButtonPressed(ActionEvent event) throws IOException {
+        mediaView.getMediaPlayer().stop();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.close();
+        mainStage.show();
+        hideTray();
+    }
+
+    private void hideTray() {
+        if (SystemTray.isSupported()) {
+            SystemTray tray = SystemTray.getSystemTray();
+            for (TrayIcon icon : tray.getTrayIcons()) {
+                tray.remove(icon);
+            }
+        }
+    }
+
+    public void pauseButtonPressed(ActionEvent event) {
+        if (isPaused) {
+            mediaView.getMediaPlayer().play();
+            isPaused = false;
+        } else {
+            mediaView.getMediaPlayer().stop();
+            isPaused = true;
+        }
+    }
+
+    public void muteCheckboxPressed(ActionEvent event) throws IOException {
+        if (muted.isSelected()) {
+            mediaView.getMediaPlayer().setMute(true);
+        } else {
+            mediaView.getMediaPlayer().setMute(false);
+        }
+
+
+    }
+}
---
+++ src/give/util/BoundUtils.java
@@ -0,0 +1,70 @@
+package give.util;
+
+import javafx.beans.value.ChangeListener;
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+/**
+ * Created on 2/25/2018 at 11:55 AM
+ *
+ * @author Nabin Bhandari
+ */
+public class BoundUtils {
+
+    // reference: https://stackoverflow.com/a/26204372/5137352
+    public static void forceStageWithinBounds(Stage stage) {
+
+        double minX = Double.POSITIVE_INFINITY;
+        double minY = Double.POSITIVE_INFINITY;
+        double maxX = Double.NEGATIVE_INFINITY;
+        double maxY = Double.NEGATIVE_INFINITY;
+
+        for (Screen screen : Screen.getScreens()) {
+            Rectangle2D screenBounds = screen.getBounds();
+            if (screenBounds.getMinX() < minX) {
+                minX = screenBounds.getMinX();
+            }
+            if (screenBounds.getMinY() < minY) {
+                minY = screenBounds.getMinY();
+            }
+            if (screenBounds.getMaxX() > maxX) {
+                maxX = screenBounds.getMaxX();
+            }
+            if (screenBounds.getMaxY() > maxY) {
+                maxY = screenBounds.getMaxY();
+            }
+        }
+
+        Bounds allScreenBounds = new BoundingBox(minX, minY, maxX - minX, maxY - minY);
+        ChangeListener<Number> boundsListener = (obs, oldValue, newValue) -> {
+            try {
+                double x = stage.getX();
+                double y = stage.getY();
+                double w = stage.getWidth();
+                double h = stage.getHeight();
+                if (x < allScreenBounds.getMinX()) {
+                    stage.setX(allScreenBounds.getMinX());
+                }
+                if (x + w > allScreenBounds.getMaxX()) {
+                    stage.setX(allScreenBounds.getMaxX() - w);
+                }
+                if (y < allScreenBounds.getMinY()) {
+                    stage.setY(allScreenBounds.getMinY());
+                }
+                if (y + h > allScreenBounds.getMaxY()) {
+                    stage.setY(allScreenBounds.getMaxY() - h);
+                }
+            } catch (StackOverflowError ignored) {
+            }
+        };
+
+        stage.xProperty().addListener(boundsListener);
+        stage.yProperty().addListener(boundsListener);
+        stage.widthProperty().addListener(boundsListener);
+        stage.heightProperty().addListener(boundsListener);
+    }
+
+}
